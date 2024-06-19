80d7afe701cf5bc4e25f8b3a33a8d84217c14bbb
gisele <108832972+giihfje@users.noreply.github.com>
2024-06-19 15:12:53 -0300

    1906

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ const textoResultado = document.querySelector('.texto-resultado');
 const lista = [item1, item2]
 const perguntas = [
     {
-        enunciado: "Pertunga 1",
-        alternativas: ["Alternativa 1", "alternativa 2"],
+        enunciado: "Assim que saiu da escola Gabriel se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, o chat também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento de Gabriel?
+",
+        alternativas: [", "alternativa 2"],
     },
     {
         enunciado: "Pertunga 2",
@@ -18,6 +19,21 @@ const perguntas = [
         alternativas: ["Alternativa 1", "alternativa 2"],
     },
 ];
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta(){
+    perguntaAtual = perguntas [atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas{
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativas;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+mostraPergunta();
 const lapis = {
     tamanho: 20,
     tipo: 'HB',
