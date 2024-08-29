2308a6247ba990ea734d7dfaa974cc3619178d1e
gisele <108832972+giihfje@users.noreply.github.com>
2024-08-29 08:32:57 -0300

    2838

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -2,3 +2,9 @@ export function aleatorio (liata){
     //Trecho de código suprimido
 }
 const nomes = ["Rubia", "Gisele"];
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()*lista.length);   
+    return lista [posicao];
+ }
+
+ export const nome = aleatorio(nomes)
